4a10e0a26cc7fc8d5f4870770b15c7b32e9382ce
gorgonzola731 <nk.kenken31@gmail.com>
2021-07-02 22:58:55 +0900

    seedデータ投入

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,30 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+category_params = [
+  {
+    name: "アプリ",
+  },
+  {
+    name: "会議"
+  }
+]
+
+Category.delete_all
+Category.create!(category_params)
+puts "categoryの初期データの投入に成功しました!"
+
+idea_params = [
+  {
+    category_id: 1,
+    body: "オンラインでブレスト",
+  },
+  {
+    category_id: 2,
+    body: "タスク管理ツール",
+  },
+
+]
+
+Idea.delete_all
+Idea.create!(idea_params)
+puts "ideaの初期データの投入に成功しました!"
+
+puts "すべての初期データ投入に成功しました!"
