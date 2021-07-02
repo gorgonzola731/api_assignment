dbe2d42e6983f28b71228418f2049a9f00b0e4f5
gorgonzola731 <nk.kenken31@gmail.com>
2021-07-02 23:03:22 +0900

    rubocopで修正

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,30 +1,30 @@
 category_params = [
   {
-    name: "アプリ",
+    name: 'アプリ'
   },
   {
-    name: "会議"
+    name: '会議'
   }
 ]
 
 Category.delete_all
 Category.create!(category_params)
-puts "categoryの初期データの投入に成功しました!"
+puts 'categoryの初期データの投入に成功しました!'
 
 idea_params = [
   {
     category_id: 1,
-    body: "オンラインでブレスト",
+    body: 'オンラインでブレスト'
   },
   {
     category_id: 2,
-    body: "タスク管理ツール",
-  },
+    body: 'タスク管理ツール'
+  }
 
 ]
 
 Idea.delete_all
 Idea.create!(idea_params)
-puts "ideaの初期データの投入に成功しました!"
+puts 'ideaの初期データの投入に成功しました!'
 
-puts "すべての初期データ投入に成功しました!"
+puts 'すべての初期データ投入に成功しました!'
